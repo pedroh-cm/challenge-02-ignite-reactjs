905c373ee46af996e50cbb104b5933196c580caa
Pedro Henrique Carlos Morinelli <pedro.morinelli@cobasi.com.br>
2023-02-28 21:06:19 -0300

    separa componentes de sideBar e Content

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,31 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button";
+
+import { GenreResponseProps } from "../App";
+
+import "../styles/sidebar.scss";
+
+interface sideBarProps {
+  genres: GenreResponseProps[];
+  selectedGenreId: number;
+  onHandleClickButton: (id: number) => void
+}
+
+export function SideBar({ genres, selectedGenreId, onHandleClickButton }:sideBarProps) {
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => onHandleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  )
 }
